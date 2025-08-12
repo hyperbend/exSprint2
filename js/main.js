13efc1552b01e0c95b9400723970422e2fa3c414
Ben Dadon <bendadon936@gmail.com>
2025-08-12 23:03:01 +0300

    phase1 step3: renderMeme with coverCanvasWithImg and centered text

--- js/main.js
+++ js/main.js
@@ -1,5 +1,36 @@
-'use strick'
+'use strict'
+
+var gElCanvas
+var gCtx
+const gImg = new Image()
 
 function onInit() {
-    console.log('App initialized - step 1 ready')
+    gElCanvas = document.getElementById('meme-canvas')
+    gCtx = gElCanvas.getContext('2d')
+    gImg.src = 'img/meme-imgs (square)/1.jpg'
+    gImg.onload = renderMeme
+}
+function renderMeme() {
+    coverCanvasWithImg(gImg)
+    drawText('Hello Meme', gElCanvas.width / 2, 20)
+}
+
+function coverCanvasWithImg(img) {
+    var CW = gElCanvas.width
+    var IW = img.naturalWidth
+    var IH = img.naturalHeight
+    var CH = Math.round(IH * CW / IW)
+    gElCanvas.height = CH
+    gCtx.drawImage(img, 0, 0, CW, CH)
+}
+
+function drawText(text, x, y) {
+    gCtx.lineWidth = 2
+    gCtx.strokeStyle = 'black'
+    gCtx.fillStyle = 'white'
+    gCtx.font = '40px Impact'
+    gCtx.textAlign = 'center'
+    gCtx.textBaseline = 'top'
+    gCtx.fillText(text, x, y)
+    gCtx.strokeText(text, x, y)
 }
