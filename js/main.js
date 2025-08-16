2a4a51cccc494cce8cfb04bf0fe415c7a865ea57
Ben Dadon <bendadon936@gmail.com>
2025-08-16 17:20:45 +0300

    phase1 step4: add gMeme model and memeService

--- js/main.js
+++ js/main.js
@@ -4,15 +4,44 @@ var gElCanvas
 var gCtx
 const gImg = new Image()
 
+const gImgs = [
+    { id: 1, url: 'img/meme-imgs (square)/1.jpg' },
+    { id: 2, url: 'img/meme-imgs (square)/2.jpg' }
+]
+
+var gMeme = {
+    selectedImgId: 2,
+    selectedLineIdx: 0,
+    lines: [
+        { txt: 'Hello Meme', size: 40, fill: 'white', stroke: 'black', x: 'center', y: 20 }
+    ]
+}
+
+function getMeme() {
+    return gMeme
+}
+
+function getImgUrlById(id) {
+    for (var i = 0; i < gImgs.length; i++) {
+        if (gImgs[i].id === id) return gImgs[i].url
+    }
+}
+
 function onInit() {
     gElCanvas = document.getElementById('meme-canvas')
     gCtx = gElCanvas.getContext('2d')
-    gImg.src = 'img/meme-imgs (square)/1.jpg'
+    gImg.src = getImgUrlById(getMeme().selectedImgId)
     gImg.onload = renderMeme
 }
+
 function renderMeme() {
     coverCanvasWithImg(gImg)
-    drawText('Hello Meme', gElCanvas.width / 2, 20)
+
+    var meme = getMeme()
+    var line = meme.lines[meme.selectedLineIdx]
+
+    var x = line.x === 'center' ? gElCanvas.width / 2 : line.x
+    drawText(line.txt, x, line.y, line.size, line.fill, line.stroke)
 }
 
 function coverCanvasWithImg(img) {
@@ -24,11 +53,11 @@ function coverCanvasWithImg(img) {
     gCtx.drawImage(img, 0, 0, CW, CH)
 }
 
-function drawText(text, x, y) {
+function drawText(text, x, y, size, fill, stroke) {
     gCtx.lineWidth = 2
-    gCtx.strokeStyle = 'black'
-    gCtx.fillStyle = 'white'
-    gCtx.font = '40px Impact'
+    gCtx.strokeStyle = stroke
+    gCtx.fillStyle = fill
+    gCtx.font = size + 'px Impact'
     gCtx.textAlign = 'center'
     gCtx.textBaseline = 'top'
     gCtx.fillText(text, x, y)
